f8f1a15d135d8ec9d339f53e0b1197bc9fa7d90b
usernamealreadynottaken <mail>
2013-09-04 18:04:55 +0200

    Holder

--- Octopus/src/octopus/view/GameView.java
+++ Octopus/src/octopus/view/GameView.java
@@ -11,31 +11,25 @@ public class GameView extends SurfaceView implements OnTouchListener, SurfaceHol
 
 	public GameView(Context context) {
 		super(context);
+		getHolder().addCallback(this);
 		
 	}
 
-	@Override
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void surfaceCreated(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void surfaceDestroyed(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean onTouch(View arg0, MotionEvent arg1) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return true;
 	}
 	
 	
