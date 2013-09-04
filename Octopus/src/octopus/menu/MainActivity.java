c7531a6bde4773ccb7e4a5ef9a79f00358dfe941
usernamealreadynottaken <mail>
2013-09-04 15:06:50 +0200

    Small changes

--- Octopus/src/octopus/menu/MainActivity.java
+++ Octopus/src/octopus/menu/MainActivity.java
@@ -1,15 +1,18 @@
 package octopus.menu;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-
 	}
 
 }
