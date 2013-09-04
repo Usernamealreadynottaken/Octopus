1f466cbe2b65e424e42a9080ef48182fc009e2ba
Altihion <Altihion@Altihion-PC>
2013-09-04 18:01:08 +0200

    GameView

---
+++ Octopus/src/octopus/view/GameView.java
@@ -0,0 +1,43 @@
+package octopus.view;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class GameView extends SurfaceView implements OnTouchListener, SurfaceHolder.Callback {
+
+	public GameView(Context context) {
+		super(context);
+		
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
